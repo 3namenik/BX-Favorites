069f3979bddf344d279c50b99fcd07efa187f856
3namenik <36602322+3namenik@users.noreply.github.com>
2022-12-13 17:21:56 +0300

    Update favorites.php

--- local/ajax/favorites.php
+++ local/ajax/favorites.php
@@ -1,56 +1,56 @@
-<?require_once($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
-if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die(); ?>
+<? require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 <?
 
 $id = $_POST['id'];
 
-switch ($_POST['action']){
-	case 'add':
-		Favorites::add($id);
-		$return['code'] = 'success';
-		$return['text'] = 'Успешно добавлено';
-		$return['items'] = Favorites::get();
-		break;
+switch ($_POST['action']) {
+    case 'add':
+        Wishlist::add($id);
+        $return['code'] = 'success';
+        $return['text'] = 'Успешно добавлено';
+        $return['items'] = Wishlist::get();
+        break;
 
-	case 'remove':
-		Favorites::remove($id);
-		$return['code'] = 'success';
-		$return['text'] = 'Успешно убрано';
-		$return['items'] = Favorites::get();		
-		break;
+    case 'remove':
+        Wishlist::remove($id);
+        $return['code'] = 'success';
+        $return['text'] = 'Успешно убрано';
+        $return['items'] = Wishlist::get();
+        break;
 
-	case 'get':
-		$return['code'] = 'success';
-		$return['items'] = Favorites::get();
-		break;
+    case 'get':
+        $return['code'] = 'success';
+        $return['items'] = Wishlist::get();
+        break;
 
-	case 'check':
-		$return['code'] = 'success';
-		if (Favorites::check($id)) {
-			$return['result'] = true;
-		} else {
-			$return['result'] = false;
-		}
-		break;
+    case 'check':
+        $return['code'] = 'success';
+        if (Wishlist::check($id)) {
+            $return['result'] = true;
+        } else {
+            $return['result'] = false;
+        }
+        break;
 
-	case 'toggle':
-		if (Favorites::check($id)) {
-			Favorites::remove($id);
-			$return['code'] = 'success';
-			$return['text'] = 'Успешно убрано';
-			$return['items'] = Favorites::get();
-		} else {
-			Favorites::add($id);
-			$return['code'] = 'success';
-			$return['text'] = 'Успешно добавлено';
-			$return['items'] = Favorites::get();
-		}
-		break;
+    case 'toggle':
+        if (Wishlist::check($id)) {
+            Wishlist::remove($id);
+            $return['code'] = 'success';
+            $return['text'] = 'Успешно убрано';
+            $return['items'] = Wishlist::get();
+        } else {
+            Wishlist::add($id);
+            $return['code'] = 'success';
+            $return['text'] = 'Успешно добавлено';
+            $return['items'] = Wishlist::get();
+        }
+        break;
 
-	case 'header_update':
-		/* Обновляем блок с количеством в шапке */
+    case 'header_update':
+        /* Обновляем блок с количеством в шапке */
 
-		break;
+        break;
 }
 echo json_encode($return);
 ?>
